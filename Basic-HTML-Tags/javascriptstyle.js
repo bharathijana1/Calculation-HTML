879f04f2c1b24d7a2af75b8cb620244eb92a7df5
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-17 12:18:27 +0530

    Add files via upload

---
+++ Basic-HTML-Tags/javascriptstyle.js
@@ -0,0 +1,256 @@
+var mesage='hello bharathi';
+// console.log(mesage);
+// console.log(12345);
+// console.log(1873.372);
+// console.log(true);
+// console.log([10,20,30,40,50]);
+// console.log({enamme:"bharathi",age:'21'});
+// console.table({enamme:"bharathi",age:'21'});
+// console.error("custum sample error");
+// console.warn("custum sample error");
+// for(i=1;i<=10;i++){
+//     console.log("value "+ i +" is: "+i);
+// }
+// var a=10,b=20,c=a+b;
+// console.log(c);
+// if(a>b){
+//     console.log("a is grater");
+// }
+// else{
+//     console.log("b is greater");
+// }
+// console.clear();
+// let ab=25;
+// console.log(ab);
+// ab=35;
+// console.log(ab);
+// const student={"name":"bharathi",'age':21};
+// console.table(student);
+// student.name="pavi";
+// console.table(student);
+// console.clear();
+
+// var num=50;
+// var des=29.4;
+// var namee="bharathi";
+// var bool=true;
+// var phoneno=null;
+// var undefine;
+// console.log(typeof num);
+// console.log(typeof des);
+// console.log(typeof namee);
+// console.log(typeof bool);
+// console.log(typeof phoneno);
+// console.log(typeof undefine);
+// var emp={
+//     "name": "bharathi",
+//     "empid":50329
+// }
+// var courses=["c","c++","java","html","css", "js"]
+// console.log(emp);
+// console.log(courses);
+// console.log(typeof emp);
+// console.log(typeof courses);
+
+// var amount=10000;
+// console.log(amount);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// amount=Number.MAX_VALUE+Number.MAX_VALUE;
+// console.log(amount);
+// amount=-10/0;
+// console.log(amount);
+// var a=10,b=20,add;
+// add=a+b;
+// var sub=b-a;
+// var mul=a*b;
+// var div=a/b;
+// var mod=11%2;
+// console.log("addition of a+b= "+add);
+// console.log("subraction of b-a= "+sub);
+// console.log("multiplication of a*b= "+mul);
+// console.log("division of a/b= "+div);
+// console.log("modulus of 11%2= "+mod);
+// //pre increment
+// var inc=11;
+// var res=++inc+3;  // 11+1=12 + 3= 15=>ans
+// console.log(res); //15
+// console.log(inc); //12
+// //post increment
+// var inc=11;
+// var res1=inc++ +3; // 11+3=14 =>ans
+// console.log(res1); // 14
+// console.log(inc);  // 12
+// console.clear();
+// var aaa=10;
+// var bbb=9;
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+// console.log(10 > 10);
+// console.log(10 < 9);
+// console.log(14 == 10);
+// console.log(10 == 10);
+// var agee=18;
+// if(18<=agee){
+//     console.log("your are ready to vote");
+// }
+// else{
+//     console.log("yoyr are not ready to vote");    
+// }
+
+
+
+
+// // var age=prompt('enter your age',18);
+// // if(age<18){
+// //     var parentpermission=confirm('you have yur parent permission right?');
+// //     if(parentpermission==true){
+// //         alert('you are allowed');
+// //     }
+// //     else{
+// //         alert('you are not allowed');
+// //     }
+// // }else if(age>60){
+// //     alert('your not allowed');
+// // }
+// // else{
+// //     alert('you are aloowed');
+// // }
+
+
+
+
+// // var month=prompt('enter a number ',6);
+// // var count=0;
+// // if(month==1 || month==3 ||month==5||month==7||month==8||month==10||month==12)
+// // {
+// //     for(let day=1;day<=31;day++){
+// //         {
+// //             count++;
+// //         }
+// // }
+// // alert(count+'days');
+ 
+// // }else if(month==4 || month==6 ||month==9||month==11){
+// //     for(let dayy=1;dayy<=30;dayy++){
+// //         count++;
+// //     }
+// //     alert(count+'days');
+// // }else{
+// //     alert('28 days');
+// // }
+
+
+
+// // do{
+// //     console.log("playing game");
+// //     var continueplaying=confirm('continue');
+// // }while(continueplaying)
+// // console.log('the end');
+
+// // var num=10;
+// // while(num>0){
+// //     console.log('num: ', num--);
+// // }
+// // console.log('while loop ended');
+// // var weel=3;
+
+// // if(weel==1)
+// // {
+// //     console.log('monday');
+// // }else if(weel==2){
+// //     console.log('tuesday');
+// // }else if(weel==3){
+// //     console.log('wednesday');
+// // }else if(weel==4){
+// //     console.log('thursday');
+// // }else if(weel==5){
+// //     console.log('friday');
+// // }else if(weel==6){
+// //     console.log('saturday');
+// // }else if(weel==7){
+// //     console.log('sunday');
+// // }
+// // var week=4;
+// // switch(week){
+// //     case 1:
+// //         console.log('monday');
+// //         break;
+// //     case 2:
+// //         console.log('tuesday');
+// //         break;
+// //     case 3:
+// //         console.log('wednesday');
+// //         break;
+// //     case 4:
+// //         console.log('thursday');
+// //         break;
+// //     case 5:
+// //         console.log('friday');
+// //         break;
+// //     case 6:
+// //         console.log('saturday');
+// //         break;
+// //     case 7:
+// //         console.log('sunday');
+// //         break;
+// //     default:
+// //         console.log('wrong input');
+// // }
+// // day=2;
+// // switch (true) {
+// //     case day>0 && day<6:
+// //         console.log('working day'); 
+// //         break;
+// //     default:
+// //         console.log('holiday');
+// //         break;
+// // }
+
+// function first(){
+//     console.log('welcome');
+// }
+// first();
+// first();
+// first();
+// function para(name,message){
+//     return message+'! '+name;
+// }
+// var generate= para('bharathi ','helllow');
+//     console.log(generate);
+//     console.log(para('pavi', 'hi'));
+
+// function paraa(mes,name){
+//     if(name){
+//         return 'hi '+ name + mes;
+//     }else{
+//         return 'hi guys ' + mes;
+//     }
+// }
+// console.log(paraa('how are you'));
+// console.log(paraa('how are you','loosu'));
+
+let str1="bharathi is a good girl";
+let str2="is it right";
+let ans=str1+str2;
+console.log(ans);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basic-HTML-Tags/newscript.js
@@ -0,0 +1,27 @@
+//  1 . background color change
+
+// let colorhexvalue = document.getElementById("colorhex");
+// let body = document.getElementById("wrap");
+// let btton = document.getElementById("btn");
+
+// let arr =[1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+
+// function randomnum(){
+//    return arr[Math.floor(Math.random()*16)];
+
+// }
+// console.log(randomnum());
+
+// btton.addEventListener('click',colorchange)
+// function colorchange(){
+// let val = "#";
+
+// for(let i=1;i<=6;i++){
+//     val += randomnum();
+// }
+// console.log(val);
+// body.style.backgroundColor = val;
+// colorhexvalue.innerHTML = val;
+// }
+
+// 2.dropdown
